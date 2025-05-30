bde8426572df8258849bf4a82910cb8f397a2659
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-30 10:57:24 -0300

    Criando arquivo biblioteca

---
+++ solução.py
@@ -0,0 +1,35 @@
+#Integranes
+#Nome: Giovane Amato dos Santos | RM: 561336
+#Nome: Lucas Figueiredo Vieira | RM: 561342
+#Nome: Matheus Roque Arantes   | RM: 561959
+import os 
+os.system("cls")
+
+
+#Menu de opções
+escolha = input(""" 
+Olá Seja bem vindo 
+O que gostaria hoje?
+
+1 - 
+2 - 
+3 - 
+4 - 
+5 - 
+""")
+
+
+#Match para a escolha do usuário
+match escolha:
+    case 1:
+      ...
+    case 2:
+      ...
+    case 3:
+      ... 
+    case 4:
+      ...  
+    case 5:
+      ...
+
+
